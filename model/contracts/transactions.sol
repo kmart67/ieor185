73110e8c36d19ab53772d8b335a2b1dc262f0c32
kmart67 <kmart67@berkeley.edu>
2018-11-11 21:58:34 -0800

    Modified contract

--- model/contracts/transactions.sol
+++ model/contracts/transactions.sol
@@ -2,22 +2,54 @@ pragma ^0.4.17;
 
 //mapping guns to owners
 contract Records {
-    mapping(uint256 => address) documentations;
+
+    struct User {
+        bool verified; //verified user iff went through background checks
+        string name;
+    }
+
+    struct Gun {
+        string name;
+        uint256 serial;
+        string manufacturer;
+        address owner;
+        bool initialized;
+    }
+
+    //Mapping to store guns.
+    mapping(uint256 => Gun) records;
+
+    event acceptedInitialization(address owner, string name, uint256 serial, string manufacturer);
+    event rejectInitialization(address owner, uint256 serial, string message);
+    event acceptedTransfer(address owner, address to, uint256 gunSerial);
+    event rejectTransfer(address owner, address to, uint256 gunSerial, string message);
+
+    //Adds gun to our records.
+    function addGun(string name, uint256 serial, string manufacturer) {
+        if (records[serial].initialized) {
+            rejectInitialization(msg.sender, serial, "This gun is already instantiated.");
+        }
+
+        records[serial] = Gun(name, serial, manufacturer, msg.sender, true);
+        acceptedInitialization(msg.sender, name, serial, manufacturer);
+    }
 
     //msg.value will be gun ID
-    function checkOwner() constant public returns (address owner) {
-        return documentations[msg.value];
+    function isOwnerOf(address owner, uint256 serial) constant public returns (bool) {
+        return (records[serial].owner == owner);
     }
 
-    function setOwner() public payable {
-        documentations[msg.value] = msg.sender;
-    }
+    //Transfers ownership of a gun.
+    function transferOwner(uint256 gunSerial, address newOwner) {
+        if (!records[gunSerial].initialized) {
+            rejectTransfer(msg.sender, newOwner, gunSerial, "No gun with this serial number exists.");
+        }
 
-    function transferOwner(uint256 gunID, address newOwner) public returns (bool success) {
-        if (documentations[gunID] != msg.sender) {
-            return false;
+        if (records[gunSerial].owner != msg.sender) {
+            rejectTransfer(msg.sender, newOwner, gunSerial, "Sender does not own this gun.");
         } else {
-            documentations[gunID] = newOwner;
+            records[gunSerial].owner = newOwner;
+            acceptedTransfer(msg.sender, newOwner, gunSerial);
         }
     }
 }
